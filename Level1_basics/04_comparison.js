7167e6f85f5e5791c8e8616ff8b55fddc7f2f1b7
Shivam Kant <97533309+myfriendshivam@users.noreply.github.com>
2025-08-05 07:47:42 +0000

    memory and strings

--- Level1_basics/04_comparison.js
+++ Level1_basics/04_comparison.js
@@ -15,3 +15,25 @@ console.log(undefined == 0); // false
 // ===  -> strictly check(datatypes also check)
 
 console.log("2" === 2); 
+
+//-------------------------Memories------------------------
+
+// Stack (primitive) -> copy of the variable , Heap(Non-primitive) -> referance of original object
+
+let myYoutube = "ShivamKantDotCom"
+let anotherName = myYoutube
+anotherName = "ShivamParam"
+console.log(myYoutube);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "shivam@gmail.com"
+
+console.log(userTwo.email);
+console.log(userOne.email);
