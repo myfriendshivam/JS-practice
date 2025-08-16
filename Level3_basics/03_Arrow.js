b1a2bf8fddfbe7be06d775a928f05e12d2a1f657
Shivam Kant <97533309+myfriendshivam@users.noreply.github.com>
2025-08-16 01:55:46 +0000

    arrow part2

--- Level3_basics/03_Arrow.js
+++ Level3_basics/03_Arrow.js
@@ -19,9 +19,35 @@ user.welcomeMessage()
 console.log(this); // refer to empty object
 
 
-function chai(){
-    console.log(this);
-    
+// function chai(){
+//     let username = "Prince"
+//     console.log(this.username); // not work in function only work in object.
+// }
+// chai()
+
+// const chai = function chai(){
+//     let username = "Prince"
+//     console.log(this.username); // not work in function only work in object.
+// }
+
+const chai = () => {
+    let username = "Prince"
+    console.log(this.username); // arrow function
 }
 
 chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1+num2
+// }
+
+// const addTwo = (num1, num2) => num1+num2 // implicit return
+// const addTwo = (num1, num2) => (num1+num2) // implicit return
+
+const addTwo = (num1, num2) => ({username: "Prince"}) // object in implicit return
+
+console.log(addTwo(3,4));
+
+const myArr = [2,5,3,8,7]
+
+myArr.forEach()
