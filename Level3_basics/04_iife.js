d4a416e66cd63bc2ae80a8b73e2d4603e2e0334c
Shivam Kant <97533309+myfriendshivam@users.noreply.github.com>
2025-08-17 18:15:21 +0000

    JS Execution Context

--- Level3_basics/04_iife.js
+++ Level3_basics/04_iife.js
@@ -10,4 +10,67 @@
     
 } )('prince')
 
-// JavaScript Exicution Context
+// JavaScript Execution Context
+
+/*
+code file  -> Global EC -> this
+ Global Execution Context
+ Function Excution Context
+ Eval Execution Context
+
+ 
+ JS Code -> Memory Creation Phase (allocate the memory)
+         -> Execution Phase
+
+let val1 = 10
+let val2 = 5
+
+function addNum(num1, num2){
+    let total = num1+num2
+    return total
+}
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
+1) Global Execution -> allocate to this
+2) Memory Phase -> 
+                val1 -> undefined
+                val2 -> undefined
+                addNum -> defination
+                result1 -> undefined
+                result2 -> undefined
+3) Execution Phase -> 
+                val1 = 10
+                val2 = 5
+                addNum -> Execution Context -> (new varable environment + execution thread)
+                                            Memory Phase
+                                            val1 -> undefined
+                                            val2 -> undefined
+                                            total -> undefined
+                                                                Excution Context
+                                                                num1 -> 10
+                                                                num2 -> 5
+                                                                total -> 15 ---> return to Global Excution Context
+                                                                                                delete the varable environment
+                                                                                                      result1 = 15
+                                                                                                              New Variable environment + thread
+                                                                                                              Memory Phase
+                                                                                                              val1 -> undefined
+                                                                                                              val2 -> undefined
+                                                                                                              total -> undefined
+                                                                                                                            execution Phase
+                                                                                                                            num1 -> 10
+                                                                                                                            num2 -> 2
+                                                                                                                            total -> 12  ---> return to Global Excution Context
+                                                                                                                                    delete the varable environment
+                                                                                                                                                result2 = 12
+  Call Stack
+  
+  
+
+
+  one() -> exectue then delete
+  Global Exec
+
+  running LIFO order
+*/
