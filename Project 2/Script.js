2a340db91d3b701820525954bf4f2b6a8bea238d
Shivam Kant <97533309+myfriendshivam@users.noreply.github.com>
2025-09-08 02:40:39 +0000

    Project 3 - DigitalClock

--- Project 2/Script.js
+++ Project 2/Script.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('form');
+// const height = parseInt(document.querySelector('#height').value); not use outside bcz it store empty value.
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const height = parseInt(document.getElementById('height').value);
-    const weight = parseInt(document.getElementById('weight').value);
-    const results = document.getElementById('results');
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
 
     if(height === '' || height <0 || isNaN(height)){
         results.innerHTML = `Please give a valid height ${height}`;
